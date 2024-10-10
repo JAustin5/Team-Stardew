218c2a6734d7c0a14f8b3e433e9e2f84acdf0b28
JAustin5 <j.da.xmaster@gmail.com>
2024-10-10 12:50:15 -0400

    adjustments of AdminScreen scripts and Login&Registration
    Admin card prefab created;
    AdminScreen script started incorporating creating, editing, and deleting card (testing needed)
    Login&Register: script Login&RegisterManager Script holds Login and Register separate scripts together in 1 script

--- UnityFiles/CofcScavengerHuntAR/Assets/Scripts/AdminMainManage.cs
+++ UnityFiles/CofcScavengerHuntAR/Assets/Scripts/AdminMainManage.cs
@@ -5,16 +5,85 @@ using UnityEngine.UI;
 
 public class AdminMainManage : MonoBehaviour
 {
-    public InputField searchbarInput;
-    // Start is called before the first frame update
-    void Start()
+    // AdminScreens Canvases
+    public GameObject adminMainScreenCanvas;
+    public GameObject locationCreationForm;
+
+    // Admin card fields
+    public GameObject adminCardPrefab;
+    public InputField locationName;
+    public InputField locationAddress;
+    public InputField coordinateLatitude;
+    public InputField coordinateLongtitude;
+    public InputField locationDescription;
+    public InputField cardHint1;
+    public InputField cardHint2;
+    public InputField cardHint3;
+    //public Image ; Getting image that is uploaded
+
+    // accumlator to keep track of number of created cards to display on Player's screen found vs not found
+    public int createdCardAcc = 0;
+
+    // Store of cards
+    public List<CardsData> cards = new List<CardsData>();
+
+    [System.Serializable]
+    public class CardsData
     {
+        public string name;
+        public string address;
+        public string latitude;
+        public string longitude;
+        public string description;
+        public string hint1;
+        public string hint2;
+        public string hint3;
+
+        public bool isUnlocked;
+    }
+
+    // Card creation (ONLY ADMIN)
+    public void CreateCard()
+    {
+        CardsData newCard = new CardsData
+        {
+            name = locationName.text,
+            address = locationAddress.text,
+            latitude = coordinateLatitude.text,
+            longitude = coordinateLongtitude.text,
+            description = locationDescription.text,
+            hint1 = cardHint1.text,
+            hint2 = cardHint2.text,
+            hint3 = cardHint3.text,
+            isUnlocked = false
+        };
+
+        cards.Add(newCard);
+        createdCardAcc++;
+
+        // TO-DO: Visual representation of card for both Admin and Player screens
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Editing an existing card
+    public void EditCard(CardsData cardData)
     {
-        
+        locationName.text = cardData.name;
+        locationAddress.text = cardData.address;
+        coordinateLatitude.text = cardData.latitude;
+        coordinateLongtitude.text = cardData.longitude;
+        locationDescription.text = cardData.description;
+        cardHint1.text = cardData.hint1;
+        cardHint2.text = cardData.hint2;
+        cardHint3.text = cardData.hint3;
+    }
+
+    // Delete an exsiting card
+    public void DeleteCard(CardsData cardData)
+    {
+        cards.Remove(cardData);
+        //TO-DO: create to destroy card itself from view
+        //Destroy();
+        createdCardAcc--;
     }
 }
--- UnityFiles/CofcScavengerHuntAR/Assets/Scripts/LoginRegisterManager.cs
+++ UnityFiles/CofcScavengerHuntAR/Assets/Scripts/LoginRegisterManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LoginRegisterManager : MonoBehaviour
@@ -18,8 +19,17 @@ public class LoginRegisterManager : MonoBehaviour
     public InputField registerEmailField;
     public InputField registerPassField;
     public InputField registerConfirmPassField;
-    
-    
+
+    public Text msg;
+    public bool validRegister = false; //USED IN BTN CLASS
+
+    // creation of account email address domains
+    private string adminDomain = "@cofc.edu";
+    private string studentDomain = "@g.cofc.edu";
+
+    // USED FOR LOGIN BTN CLASS
+    public bool correctInfo = false;
+
     public void ShowRegister()
     {
         loginCanvas.SetActive(false);
@@ -31,16 +41,114 @@ public class LoginRegisterManager : MonoBehaviour
         loginCanvas.SetActive(true);
         registerCanvas.SetActive(false);
     }
-    
-    // Start is called before the first frame update
-    void Start()
+
+    public void Login()
     {
-        ShowLogin();
+        string email = loginEmailField.text;
+        string password = loginPassField.text;
+
+        //if (!IsValidEmailDomain(email))
+        //{
+        //    msg.text = "Email must be from a valid CofC email address";
+        //    return;
+        //}
+
+        if (PlayerPrefs.HasKey(email + "_password"))
+        {
+            string savedPassword = PlayerPrefs.GetString(email + "_password");
+            string userRole = PlayerPrefs.GetString(email + "_role");
+
+            if (savedPassword == password)
+            {
+                msg.text = "Login successful!";
+                if (userRole == "Admin")
+                {
+                    SceneManager.LoadScene("AdminLocationHandle");
+                }
+                else
+                {
+                    SceneManager.LoadScene("MainScene");
+                }
+            }
+            else
+            {
+                msg.text = "Incorrect password. Try again.";
+            }
+        }
+        else
+        {
+            msg.text = "Email not found. Please register or try again.";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool getLoginValidity()
     {
-        
+        return correctInfo;
     }
+
+
+    public void Register()
+    {
+        string name = registerNameField.text;
+        string email = registerEmailField.text;
+        string password = registerPassField.text;
+        string confirmPassword = registerConfirmPassField.text;
+        string role = "Student";
+
+        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmPassword))
+        {
+            msg.text = "Please fill in all fields.";
+            return;
+        }
+
+        // password confirmation
+        if (password != confirmPassword)
+        {
+            msg.text = "Registration Failed: Passwords do not match. Try again?";
+            return;
+        }
+
+        // email check if admin or student
+        if (email.EndsWith(adminDomain))
+        {
+            role = "Admin";
+        }
+        else if (email.EndsWith(studentDomain))
+        {
+            role = "Student";
+        }
+        else
+        {
+            msg.text = "Email address is not allowed for registration. Please try again with a valid CofC email address.";
+            return;
+        }
+
+        if (PlayerPrefs.HasKey(email))
+        {
+            msg.text = "Email already exists. Try again with a different CofC email.";
+        }
+        else
+        {
+            PlayerPrefs.SetString(email + "_password", password);
+            PlayerPrefs.SetString(email + "_role", role);
+            msg.text = "Regstration successful as " + role + "!";
+        }
+    }
+
+    public bool getRegValidity() //ADD BOOL CHANGE IN REGISTER ATTEMPT
+    {
+        return validRegister;
+    }
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    ShowLogin();
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
 }
